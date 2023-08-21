f7788ac3e33b1fe08774c6d351dbda6c64e9aab8
hippocookie <zesheng.zong@outlook.com>
2023-08-21 23:57:24 +0800

    Add package info

---
+++ graphx/src/main/scala/package.scala
@@ -0,0 +1,10 @@
+package org.apache.spark
+
+import org.apache.spark.util.collection.OpenHashSet
+package object graphx {
+  type VertexId = Long
+
+  type PartitionID = Int
+
+  private[graphx] type VertexSet = OpenHashSet[VertexId]
+}
